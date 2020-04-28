13a5b19f32cf11b48b1348155259e2504b517254
Denis <carriere.denis@gmail.com>
2020-04-27 22:22:18 -0400

    update deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,9 +4,9 @@ mkdir -p dist
 cleos wallet unlock --password $(cat ~/eosio-wallet/.pass)
 
 # create accounts
-cleos create account eosio pingpong.sx EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV
-cleos create account eosio myaccount EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV
-cleos create account eosio toaccount EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV
+cleos system newaccount eosio pingpong.sx EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV --stake-net "1.0000 EOS" --stake-cpu "5.0000 EOS" --buy-ram "100.0000 EOS"
+cleos system newaccount eosio myaccount EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV --stake-net "1.0000 EOS" --stake-cpu "5.0000 EOS" --buy-ram "100.0000 EOS"
+cleos system newaccount eosio toaccount EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV --stake-net "1.0000 EOS" --stake-cpu "5.0000 EOS" --buy-ram "100.0000 EOS"
 
 # deploy smart contract
 cleos set contract pingpong.sx . pingpong.sx.wasm pingpong.sx.abi
